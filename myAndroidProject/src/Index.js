4b0a4fec71d16a3a7e31ccca0c5046b11d6a57bc
Sai Rashwant <sairashwantno1@gmail.com>
2022-01-25 12:45:44 +0530

    First Commit

--- myAndroidProject/src/Index.js
+++ myAndroidProject/src/Index.js
@@ -1,42 +1,115 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, Button, StatusBar} from 'react-native';
 import SaiRashwant from './CustomText';
-
 export default function login()
 {
+  const [counter,setCounter] = useState(0);
+  const increment = () => {
+    setCounter(counter+1);
+  }
+  const decrement = () => {
+    setCounter(counter-1);
+  }
+  const reset = () => {
+    setCounter(count=0);
+  }
     return(
     <View style={styles.container}>
-      <Image
-      source={require("../assets/login.png")}
-      style={{width:200,height:200}}
+      <Text style={styles.welcome}>Counter App</Text>
+      <View style={styles.img}>
+      <Image 
+      source={require("../assets/counterlogo.png")}
+      style={styles.img}
       />
-
-      <Text style={styles.welcome}>Login Page</Text>
-      <SaiRashwant placeholder="Username" security={false}/>
-      <SaiRashwant placeholder="Password" security={true}/>
-
-    <View style={styles.buttonspace}>
-      <TouchableOpacity>
-      <Text style={styles.boxcolor}>   Login   </Text>
-      </TouchableOpacity>
-      <TouchableOpacity>
-      <Text style={styles.boxcolor}>   Signup   </Text>
-      </TouchableOpacity>
-      </View>  
-
     </View>
+      
+      
+
+      <View style={styles.extra}>
+      <Text style={styles.welcome2}>{counter}</Text>
+      </View>
+      
+
+    
+      
+    <View style={styles.bg}>
+
+    
+      
+    <View style={styles.buttonspace}>
+
+    
+      
+    <Button
+    onPress={increment}
+    title="Increment"
+    color="red"
+    accessibilityLabel="Learn more about this purple button"
+    />
+
+<Button
+    onPress={reset}
+    title="RESET"
+    color="#808080  "
+    accessibilityLabel="Learn more about this purple button"
+    />
+
+
+      <Button
+      
+      marginStart="40"
+      onPress={decrement}
+      flexDirection="row"
+      title="Decrement"
+      color="black"
+      accessibilityLabel="Learn more about this purple button"
+      />
+    </View>
+    </View>
+  
+    </View>
+    
+    
 );
 
 }
 const styles = StyleSheet.create({
-  
+  bg2:
+  {
 
+    backgroundColor:'red',
+    marginStart:10
+  },
+  
+  bg: {
+    
+    backgroundColor: "#FF69B4",
+    paddingVertical: 5,
+    paddingHorizontal: 5,
+    marginTop:140,
+    marginStart:-15,
+    borderTopLeftRadius:165,
+    borderTopRightRadius:140,
+
+  },
+  img:{
+      width:200,
+      height:200,
+      shadowColor: '#202020',
+      shadowOffset: {width: 10, height: 10},
+      shadowOpacity:0.3,
+      shadowRadius: 5,
+
+  },
+
+ 
     container: {
   
-      flex: 1,
+      
+      marginTop:10,
   
-      backgroundColor: 'cyan',
+      backgroundColor: 'white',
   
       alignItems: 'center',
   
@@ -44,12 +117,41 @@ const styles = StyleSheet.create({
   
     },
     welcome: {
-      
       fontSize:50,
       textAlign:'center',
       margin:10,
-      color:"blue",
-      fontFamily:"Neonderthaw-Regular"
+      
+      color:"red",
+      fontFamily:"MochiyPopPOne-Regular",
+      textShadowOffset:
+      {
+        width:5,
+        height:5,
+      },
+      textShadowRadius: 0.1,
+      textShadowColor: 'grey',
+      
+      
+      
+    },
+    welcome2: {
+      marginTop:10,
+      fontSize:50,
+      textAlign:'center',
+      margin:10,
+      
+      color:"red",
+      fontFamily:"MochiyPopPOne-Regular",
+      textShadowOffset:
+      {
+        width:5,
+        height:5,
+      },
+      textShadowRadius: 0.1,
+      textShadowColor: 'grey',
+      
+      
+      
     },
     input:
     {
@@ -58,19 +160,26 @@ const styles = StyleSheet.create({
         padding:15,
         marginBottom:10
     },
-    boxcolor:
-    {
-      backgroundColor:"yellow",
-      width:"100%",
-      fontSize:30,
-      width:"90%"
-      
   
-    },
     buttonspace:
     {
+      
+      
       flexDirection:"row",
-      justifyContent:"space-between"
+      
+      justifyContent:"space-between",
+      padding:"10%",
+      marginTop:10,
+      marginStart:70,
+      width:"100%"
+    },
+    extra:
+    {
+      marginStart:10,
+      width:300,
+      marginTop:50,
+      backgroundColor:"#FFC0CB",
+      borderRadius: 10
     }
   
   
